89371d16244d1b57a85cf6826f157510d24ee481
marumaru1019 <ryu.ryusei401@gmail.com>
2021-06-29 00:26:19 +0900

    create show&index&delete

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,68 @@
+class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :update]
+
+  def index
+    recipes = Recipe.select("id",
+                            "title",
+                            "making_time",
+                            "serves",
+                            "ingredients",
+                            "cost"
+                          ).order(created_at: :asc)
+    render json: { recipes: recipes }
+  end
+
+  def show
+    render json: { message: 'Recipe details by id', recipe: @recipe }
+  end
+
+  def create
+    recipe = Recipe.new(recipe_params)
+    if recipe.save
+      render json: { message: 'Recipe successfully created!', recipe: recipe }
+    else
+      render json: { message: 'Recipe creation failed!', required: "title, making_time, serves, ingredients, cost" }
+    end
+  end
+
+  def destroy
+      recipe = Recipe.find_by(id: params[:id])
+      if recipe
+        recipe.destroy
+        render json: { message: 'Recipe successfully removed!' }
+      else
+        render json: { message: "No Recipe found" }
+      end
+  end
+
+  def update
+    recipe = Recipe.select(
+                            "title",
+                            "making_time",
+                            "serves",
+                            "ingredients",
+                            "cost"
+                          ).find(params[:id])
+    if @recipe.update(recipe_params)
+      render json: { message: 'Recipe successfully updated!', recipe: recipe }
+    else
+      render json: { message: 'Recipe updating failed!', required: "title, making_time, serves, ingredients, cost" }
+    end
+  end
+
+  private
+
+  def set_recipe
+        @recipe = Recipe.select("id",
+                                "title",
+                                "making_time",
+                                "serves",
+                                "ingredients",
+                                "cost"
+                              ).find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :making_time, :serves, :ingredients, :cost)
+  end
+end
---
+++ app/models/recipe.rb
@@ -0,0 +1,7 @@
+class Recipe < ApplicationRecord
+  validates :title, presence: true
+  validates :making_time, presence: true
+  validates :serves, presence: true
+  validates :ingredients, presence: true
+  validates :cost, presence: true
+end
